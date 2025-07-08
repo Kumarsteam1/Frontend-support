9fc854c78f6e28e9f12403da7a5aab6772ce363e
Kumarswammy <kumarswamyshinagam8@gmail.com>
2025-07-08 10:09:33 +0530

    Second Commit

--- index.js
+++ index.js
@@ -1,19 +1,31 @@
 // index.js
 
+// Get all radio buttons
 const radioInputs = document.querySelectorAll('input[type="radio"]');
+
+// Get boxes and total display element
 const boxes = document.querySelectorAll('.box');
 const total = document.querySelector('.total');
 
+// Define prices for each option
 const prices = {
   1: '$10.00 USD',
   2: '$18.00 USD',
   3: '$24.00 USD'
 };
 
-radioInputs.forEach((radio, idx) => {
+// Listen for changes in selection
+radioInputs.forEach((radio, i) => {
   radio.addEventListener('change', () => {
-    boxes.forEach(box => box.classList.remove('selected'));
-    boxes[idx].classList.add('selected');
-    total.innerText = `Total: ${prices[radio.value]}`;
+    // Remove 'selected' class from all boxes
+    boxes.forEach(box => {
+      box.classList.remove('selected');
+    });
+
+    // Add 'selected' class to clicked one
+    boxes[i].classList.add('selected');
+
+    // Update total price text
+    total.innerText = 'Total: ' + prices[radio.value];
   });
 });
